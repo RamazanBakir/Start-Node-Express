fbb0fcd68cc9dcc55b0e5a802c80ec14ff10f785
Ramazan Bakır <48602486+RamazanBakir@users.noreply.github.com>
2020-12-13 16:32:29 +0300

    Express Simple RestAPI

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
 const express = require('express'); //import express
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev')); //print additional info to screen as required.
+app.use(bodyParser.json());
+
+app.all('/dishes',(req,res,next) =>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');//Content-Type we set it up different.
+    next();//next,means complete all endpoint down below here
+});//endpoint
+app.get('/dishes',(req,res,next) =>{
+    res.end('Will send all the dishes to you ! ');
+}); 
+app.post('/dishes',(req,res,next) =>{
+    res.end('Will add the dish : '+ req.body.name +' with details : ' +req.body.description);
+}); 
+app.put('/dishes',(req,res,next) =>{
+    res.statusCode = 403; //403 operation is not supported.
+    res.end('PUT operation is not supported on /dishes');
+}); 
+app.delete('/dishes',(req,res,next) =>{
+    res.end('Deleting all the dishes !');
+}); 
+app.get('/dishes/:dishId',(req,res,next) =>{
+    res.end('Will send details of the dish: '
+    +req.params.dishId + 'to you !');
+}); 
+app.post('/dishes/:dishId',(req,res,next) =>{
+    res.statusCode = 403; //403 operation is not supported.
+    res.end('POST operation is not supported on /dishes/'
+    +req.params.dishId);
+}); 
+app.put('/dishes/:dishId',(req,res,next) =>{
+    res.write('Updating the dish: '+req.params.dishId+'\n');
+    res.end('Will update the dish: '+req.body.name + ' with details '+ req.body.description);
+}); 
+app.delete('/dishes/:dishId',(req,res,next) =>{
+    res.end('Deleting dish: '+req.params.dishId);
+}); 
 
 app.use(express.static(__dirname+ '/public'));
 
 app.use((req,res,next) => {
     //console.log(req.headers); we remove this because morgan is print it out.
-
     res.statusCode=200;
     res.setHeader('Content-Type','text/html');
     res.end('<html><body><h1>This is an Express Server </h1></body></html>');
 });
-
 
 const server = http.createServer(app);
 server.listen(port,hostname,() =>{
