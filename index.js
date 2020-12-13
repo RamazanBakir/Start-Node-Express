785dc0301be3c64ba3f3f0ce9c7b829f7d8016cd
Ramazan Bakır <48602486+RamazanBakir@users.noreply.github.com>
2020-12-13 15:26:24 +0300

    Express Serve Static Files

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express'); //import express
 const http = require('http');
- 
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev')); //print additional info to screen as required.
+
+app.use(express.static(__dirname+ '/public'));
 
 app.use((req,res,next) => {
-    console.log(req.headers);
+    //console.log(req.headers); we remove this because morgan is print it out.
+
     res.statusCode=200;
     res.setHeader('Content-Type','text/html');
     res.end('<html><body><h1>This is an Express Server </h1></body></html>');
