a949c478281faf452bf0986b6a54e44ad11e1624
Ramazan Bakır <48602486+RamazanBakir@users.noreply.github.com>
2020-12-13 16:49:08 +0300

    RestAPI support using express router

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -10,24 +11,8 @@ const app = express();
 app.use(morgan('dev')); //print additional info to screen as required.
 app.use(bodyParser.json());
 
-app.all('/dishes',(req,res,next) =>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');//Content-Type we set it up different.
-    next();//next,means complete all endpoint down below here
-});//endpoint
-app.get('/dishes',(req,res,next) =>{
-    res.end('Will send all the dishes to you ! ');
-}); 
-app.post('/dishes',(req,res,next) =>{
-    res.end('Will add the dish : '+ req.body.name +' with details : ' +req.body.description);
-}); 
-app.put('/dishes',(req,res,next) =>{
-    res.statusCode = 403; //403 operation is not supported.
-    res.end('PUT operation is not supported on /dishes');
-}); 
-app.delete('/dishes',(req,res,next) =>{
-    res.end('Deleting all the dishes !');
-}); 
+app.use('/dishes',dishRouter);
+//alt kısmı kes 
 app.get('/dishes/:dishId',(req,res,next) =>{
     res.end('Will send details of the dish: '
     +req.params.dishId + 'to you !');
@@ -44,7 +29,7 @@ app.put('/dishes/:dishId',(req,res,next) =>{
 app.delete('/dishes/:dishId',(req,res,next) =>{
     res.end('Deleting dish: '+req.params.dishId);
 }); 
-
+//üst kısmı kes 
 app.use(express.static(__dirname+ '/public'));
 
 app.use((req,res,next) => {
